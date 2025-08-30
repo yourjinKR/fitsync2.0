c6d4bd3cfe68113d5ef0bef884593336c528e01a
yourjin <you720223721@gmail.com>
2025-08-30 23:27:15 +0900

    Initial commit

---
+++ backend/src/test/java/com/fitsync/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
