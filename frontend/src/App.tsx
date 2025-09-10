a38d7eb69f5a78c3f2b06c40bcedd795ad21862f
yourjin <you720223721@gmail.com>
2025-09-10 21:17:14 +0900

    style : 글로벌 스타일 임시 구현 다크모드, oklch, 기본 form 정의

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Display from './Display';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle/>
       <Display/>
     </BrowserRouter>
   );
