530294d2a1e97ae8f87a188c13688efcefdf03ea
yourjin <you720223721@gmail.com>
2025-09-15 02:40:12 +0900

    fix : base url 테스트

--- frontend/src/api/AuthApi.ts
+++ frontend/src/api/AuthApi.ts
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.BACKEND_URL || 'http://localhost:8080';
+export const baseURL = import.meta.env.BACKEND_URL || 'http://localhost:8080';
 
 export const GOOGLE_URL = `${baseURL}/oauth2/authorization/google`;
 export const NAVER_URL = `${baseURL}/oauth2/authorization/naver`;
