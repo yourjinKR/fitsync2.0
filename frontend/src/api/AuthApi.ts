bfcfee9cd2c9fad4debb0b9afca1805b0a434af5
yourjin <you720223721@gmail.com>
2025-09-16 00:29:31 +0900

    fix : backend_url 변수명 수정

--- frontend/src/api/AuthApi.ts
+++ frontend/src/api/AuthApi.ts
@@ -1,5 +1,5 @@
-export const backendURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080';
 
-export const GOOGLE_URL = `${backendURL}/oauth2/authorization/google`;
-export const NAVER_URL = `${backendURL}/oauth2/authorization/naver`;
-export const KAKAO_URL = `${backendURL}/oauth2/authorization/kakao`;
+export const GOOGLE_URL = `${BACKEND_URL}/oauth2/authorization/google`;
+export const NAVER_URL = `${BACKEND_URL}/oauth2/authorization/naver`;
+export const KAKAO_URL = `${BACKEND_URL}/oauth2/authorization/kakao`;
