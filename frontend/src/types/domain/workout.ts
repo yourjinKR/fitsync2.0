bee1f8d21e0aeeea9efe18a769d19778a1a2c5d5
yourjin <you720223721@gmail.com>
2025-10-09 16:40:30 +0900

    fix : ts에 jsonb 타입을 위한 JSON 타입 추가

--- frontend/src/types/domain/workout.ts
+++ frontend/src/types/domain/workout.ts
@@ -2,6 +2,7 @@
 // DTOs for Request (Client -> Server)
 // ============================
 
+import { JSONObject } from "../common";
 import { RoutineDetailResponseDto } from "./routine";
 
 // Omit을 사용해 Response 타입으로부터 생성
@@ -11,13 +12,6 @@ type WorkoutSetRequestDto = Omit<WorkoutSetResponseDto, 'id'>;
 type WorkoutExerciseRequestDto = Omit<WorkoutExerciseResponseDto, 'id' | 'workoutSets'> & {
   workoutSets: WorkoutSetRequestDto[];
 };
-
-// 루틴 스냅샷 객체의 형태를 구체적으로 정의
-interface RoutineSnapshotDetail {
-  sets: number;
-  reps: number;
-  // ... 기타 필요한 속성들
-}
 
 export interface WorkoutCreateRequestDto {
   title: string;
@@ -57,7 +51,7 @@ interface WorkoutExerciseResponseDto {
 export interface WorkoutDetailResponseDto {
   id: number;
   title: string;
-  routineSnapshot: { [key: string]: RoutineSnapshotDetail }; // Map 대신 객체 + 인덱스 시그니처 사용
+  routineSnapshot: JSONObject;
   memo: string;
   createdAt: string; // 서버에서 받은 후 new Date(createdAt)으로 변환해서 사용하는 것을 추천
   owner: SimpleUserDto;
