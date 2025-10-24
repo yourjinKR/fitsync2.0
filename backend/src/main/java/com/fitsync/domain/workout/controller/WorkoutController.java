dde3fe68f5cd39cf290396fe6f91c6a21f4ccd73
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-10-25 00:40:17 +0900

    style(workoutController): 메서드명 수정 및 미사용 import 삭제

--- backend/src/main/java/com/fitsync/domain/workout/controller/WorkoutController.java
+++ backend/src/main/java/com/fitsync/domain/workout/controller/WorkoutController.java
@@ -4,10 +4,8 @@ import com.fitsync.domain.workout.dto.WorkoutCreateRequest;
 import com.fitsync.domain.workout.dto.WorkoutDetailResponse;
 import com.fitsync.domain.workout.dto.WorkoutSimpleResponse;
 import com.fitsync.domain.workout.dto.WorkoutUpdateRequest;
-import com.fitsync.domain.workout.entity.Workout;
 import com.fitsync.domain.workout.service.WorkoutService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +20,7 @@ public class WorkoutController {
     private final WorkoutService workoutService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<WorkoutDetailResponse> getWorkoutById(@PathVariable Long id) {
+    public ResponseEntity<WorkoutDetailResponse> getWorkout(@PathVariable Long id) {
 
         WorkoutDetailResponse responseDto = workoutService.getWorkoutById(id);
 
