2302f167e1667893ef12c6f7f16014ad806d5a8d
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-05 03:36:39 +0900

    feat(ErrorMessage) : 에러 메세지 통합 관리 부가적인 내용 담기 지원

---
+++ backend/src/main/java/com/fitsync/global/error/ErrorMessage.java
@@ -0,0 +1,23 @@
+package com.fitsync.global.error;
+
+import java.text.MessageFormat;
+import lombok.Getter;
+
+@Getter
+public enum ErrorMessage {
+    ERROR("기본 에러"),
+    EXERCISE_DUPLICATE_NAME("이미 동일한 이름의 운동이 존재합니다."),
+    EXERCISE_NOT_FOUND("해당 ID와 일치하는 {0} 정보를 찾지 못했습니다. (id: {1})");
+    ;
+
+    private final String message;
+    private static final String PREFIX = "[ERROR]";
+
+    ErrorMessage(String message) {
+        this.message = MessageFormat.format("{0} {1}", PREFIX, message);
+    }
+
+    public String format(Object... args) {
+        return MessageFormat.format("{0} {1}", PREFIX, MessageFormat.format(message, args));
+    }
+}
